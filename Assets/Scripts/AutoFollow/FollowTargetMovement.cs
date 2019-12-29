3be6d167fcee06fcc469a66ebce5e5c6aae0849e
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2019-12-30 00:44:14 +0200

    Dogs Follow
    Dogs follows with 15steps.
    dogs teleports(lerps in 15 steps) to the human (for now) . glitchless

--- Assets/Scripts/AutoFollow/FollowTargetMovement.cs
+++ Assets/Scripts/AutoFollow/FollowTargetMovement.cs
@@ -7,7 +7,7 @@ public class FollowTargetMovement : MonoBehaviour
     public Transform target;
     PlayerMovement playerMovement;
     public Vector3 offset;
-    Vector3 oldposition;
+    //Vector3 oldposition;
     Vector3 nowPosition;
     public List<TargetPoint> points;
     float timer;
@@ -33,13 +33,19 @@ public class FollowTargetMovement : MonoBehaviour
     {
         playerMovement = target.GetComponent<PlayerMovement>();
         timer=0;
-        tickTimer=0;
+        //tickTimer=0;
+        //nowPosition = transform.position;
+        //oldposition = target.position;
+    }
+
+    void OnEnable(){
         nowPosition = transform.position;
-        oldposition = target.position;
+        timer=0;
         points = new List<TargetPoint>{};
-        for (int i = 0; i < LeaveSteps; i++)
+        for (int i = 1; i < LeaveSteps+1; i++)
         {
-            points.Add(new TargetPoint((target.position-transform.position)/LeaveSteps, 0, true, true));
+            //points.Add(new TargetPoint(transform.position+(target.position-transform.position)*(Vector3.Distance(target.position,transform.position)*1/i), 0, true, true));
+            points.Add(new TargetPoint(Vector3.Lerp(transform.position-offset, target.position, 0.1f*i), 0, true, true));
         }
     }
 
@@ -48,11 +54,11 @@ public class FollowTargetMovement : MonoBehaviour
     {
         points.Add(new TargetPoint(target.position, timer, IsGrounded(), playerMovement.lookingLeft));
         timer=0;
-        oldposition = target.position;
+        //oldposition = target.position;
         if (points.Count>LeaveSteps){
             PlayNextStep();
         }
-        transform.position = Vector3.Lerp(transform.position, nowPosition, 1);
+        transform.position = Vector3.Lerp(transform.position, nowPosition, 0.8f);
         Vector3 v = points[0].lookingLeft? new Vector3(0f, 0f, 1f) : new Vector3 (0f, 0f, -1f);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(v), Time.deltaTime * 10);
     }
