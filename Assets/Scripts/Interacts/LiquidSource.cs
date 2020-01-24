0b03a4b0a4f6680ae15ed257a64ddf648661c72b
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-01-24 21:38:21 +0200

    Water soucre and Bucket
    Added "LiquidSource" script, it saves the liquid's prefabs, name and  prefabs when in containers.
    OnHoldInteract, human with bucket can take water from source and throw the water OnThrow of the bucket.
    (Small changes to make this interaction)
    Added water source prefab.
    File management.

---
+++ Assets/Scripts/Interacts/LiquidSource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidSource : MonoBehaviour
+{
+    public string liquidName;
+    public GameObject[] contentPrefab; //prefab as how the liquid will look inside the container. 0: bucket
+    public GameObject[] liquidPrefab;
+
+    bool GetLiquid(Transform t){
+        bool flag = false;
+        InteractControl i = t.GetComponent<InteractControl>();
+        if (i){
+            if (i.IsHuman){
+                if (i.rightGrab){
+                Bucket b = i.rightGrab.GetComponentInChildren<Bucket>();
+                    if (b){
+                        flag = true;
+                        b.FillBucketWith(liquidName, contentPrefab[0], liquidPrefab[0]);
+                    }
+                }
+            }
+        } 
+        return flag;
+    }
+
+    void OnHoldInteract(MessageArgs msg)
+    {
+        msg.received = GetLiquid(msg.sender);
+
+    }
+}
--- Assets/Scripts/Items/Bucket.cs
+++ Assets/Scripts/Items/Bucket.cs
@@ -5,25 +5,54 @@ using UnityEngine;
 public class Bucket : MonoBehaviour
 {
     bool filled;
+    public string liquidName;
     public GameObject contentPrefab;
+    public GameObject liquidPrefab;
+    public Transform contentParent;
 
     void Start(){
-        filled = true;
+        filled = false;
+        liquidName = null;
+        contentPrefab = null;
+        liquidPrefab = null;
     }
 
     bool ThrowContent(Transform t){
         bool flag = false;
-        if (filled){
+        InteractControl i = t.GetComponent<InteractControl>();
+        if (filled && i.IsHuman){
             filled = false;
             flag = true;
-            GameObject g = GameObject.Instantiate(contentPrefab, transform.position, Quaternion.identity);
-            InteractControl i = t.GetComponent<InteractControl>();
+
+            GameObject g = GameObject.Instantiate(liquidPrefab, transform.position, Quaternion.identity);
             Vector3 throwingAngle = i.throwingAngle;
             float throwPower = i.throwPower;
             float throwForce = i.throwForce;
             g.GetComponent<Rigidbody>().AddForce(throwingAngle * throwPower * throwForce, ForceMode.Impulse);
+
+            liquidName = null;
+            SetContent(null);
+            liquidPrefab = null;
         }
         return flag;
+    }
+
+    public void FillBucketWith(string n, GameObject c, GameObject l){
+        filled = true;
+        liquidName = n;
+        SetContent(c);
+        liquidPrefab = l;
+    }
+
+    void SetContent(GameObject c){
+        if (contentParent.childCount>0){
+            Destroy(contentParent.GetChild(0).gameObject);
+        }
+        contentPrefab = c;
+        if (c!=null){
+            GameObject g = GameObject.Instantiate(c, contentParent.position, contentParent.rotation);
+            g.transform.SetParent(contentParent);
+        }
     }
 
     void OnThrow(MessageArgs msg){
