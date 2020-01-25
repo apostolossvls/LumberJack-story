4473395833b87decda7af76b7ff392db29fc6caa
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-01-25 14:26:17 +0200

    Fire and Water
    Added "Water" and "Fire" tags.
    Added "Fire1" prefab.  its a flame with 2 colliders, a damageDealerOnTrigger that damages over time.
    Water particles reduce the power of the fire OnParticleCollision.

---
+++ Assets/Scripts/Interacts/Fire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float power=1f;
+
+    void Update(){
+        if (power<=0){
+            PutOut();
+        }
+    }
+
+    public void SetPower(float amount, bool fixedNumber=false){
+        if (!fixedNumber){
+            power += amount;
+        }
+        else {
+            power = amount;
+        }
+    }
+
+    public void PutOut(){
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Interacts/Water.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.tag=="Fire"){
+            Fire fire = other.GetComponent<Fire>();
+            if (fire){
+                fire.SetPower(-Time.deltaTime*10f);
+            }
+        }
+    }
+    /*void OnTriggerStay(Collider other){
+        if (other.tag=="Fire"){
+            Debug.Log("water found fire");
+            Fire fire = other.GetComponent<Fire>();
+            if (fire){
+                fire.SetPower(-Time.deltaTime*10f);
+                Debug.Log("water found fire script");
+            }
+        }
+    }*/
+}
