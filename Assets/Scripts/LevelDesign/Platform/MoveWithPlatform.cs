4bfc11d289da29676dfe2db2f796e28d7be17a31
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-01-28 23:37:27 +0200

    Lantern
    Added Lantern prefab. Its a source of light (point light).
    Changed level to test lantern.

--- Assets/Scripts/LevelDesign/Platform/MoveWithPlatform.cs
+++ Assets/Scripts/LevelDesign/Platform/MoveWithPlatform.cs
@@ -15,16 +15,18 @@ public class MoveWithPlatform : MonoBehaviour
     public bool automatic;
     public string[] tags;
     List<Transform> obj;
+    Rigidbody rig;
 
     void Start(){
+        rig = GetComponent<Rigidbody>();
         obj = new List<Transform>{};
         if (points.Length > 0){
             currentTarget = points[0];
         }
-        tolerance = speed * Time.deltaTime;
+        tolerance = speed * Time.fixedDeltaTime;
     }
 
-    void LateUpdate(){
+    void FixedUpdate(){
         if (transform.localPosition!=currentTarget){
             MovePlatform();
         }
@@ -32,6 +34,7 @@ public class MoveWithPlatform : MonoBehaviour
             dif=Vector3.zero;
             UpdateTarget();
         }
+        /*
         for (int i = 0; i < obj.Count; i++)
         {
             if (obj[i]==null) obj.RemoveAt(i);
@@ -41,13 +44,15 @@ public class MoveWithPlatform : MonoBehaviour
                 obj[i].position += dif;
             }
         }
+        */
     }
 
     void MovePlatform(){
         Vector3 heading = currentTarget - transform.localPosition;
+        rig.MovePosition(transform.position+heading.normalized * speed * Time.fixedDeltaTime);
         //Vector3.Lerp (movingPlatform.position, newPostion, smooth * Time.deltaTime) ;
-        transform.localPosition += heading.normalized * speed * Time.deltaTime;
-        dif = heading.normalized * speed * Time.deltaTime;
+        //transform.localPosition += heading.normalized * speed * Time.fixedDeltaTime;
+        dif = heading.normalized * speed * Time.fixedDeltaTime;
 
         //transform.localPosition = Vector3.Lerp(transform.localPosition, currentTarget, speed * Time.fixedDeltaTime);
         //dif = transform.localPosition - lastPos;
@@ -78,10 +83,19 @@ public class MoveWithPlatform : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other){
-        if (TagMatch(other.tag) && OnArray(other.transform)==-1) obj.Add(other.transform);
+        if (TagMatch(other.tag)) {
+        //if (TagMatch(other.tag) && OnArray(other.transform)==-1) {
+            //obj.Add(other.transform);
+            other.transform.parent = transform;
+        }
     }
     void OnTriggerExit(Collider other){
-        if (OnArray(other.transform)!=-1) obj.Remove(other.transform);
+        /*if (OnArray(other.transform)!=-1) {
+            obj.Remove(other.transform);
+        }*/
+        if (other.transform.parent == transform) {
+            other.transform.parent = null;
+        }
     }
     bool TagMatch (string s){
         foreach (string t in tags)
