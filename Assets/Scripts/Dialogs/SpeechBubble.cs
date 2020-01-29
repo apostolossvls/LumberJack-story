4414e25b1ef47a8c41adf670e3ec73f9e694cbc4
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-01-29 21:20:29 +0200

    Speech Bubbles
    Speech Bubbles was added.
    It has "Speech Bubbles" script that takes a string and outputs it the the enabled text script (TextMeshPro or UI.Text). With speed 0 it will print intant and with speed increasing it will print characters one by one (speed=1:20charsPerSec, speed=2:40charsPerSec etc)

---
+++ Assets/Scripts/Dialogs/SpeechBubble.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeechBubble : MonoBehaviour
+{
+    [TextArea(15,20)]
+    public string message;
+    public TextMeshPro textMeshPro;
+    public Text textUI;
+    public Image bubbleImage;
+    public float speed; //0 instant
+    int charIndex;
+
+    void OnEnable()
+    {
+        
+        if (speed==0){
+            if (textMeshPro.isActiveAndEnabled) textMeshPro.text = message;
+            else if (textUI.isActiveAndEnabled) textUI.text = message;
+            else return;
+        }
+        else {
+            if (textMeshPro.isActiveAndEnabled) textMeshPro.text = "";
+            else if (textUI.isActiveAndEnabled) textUI.text = "";
+            else return;
+            charIndex = 0;
+        }
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText(){
+        if (speed!=0){
+            charIndex = 0;
+            while (charIndex < message.Length){
+                if (textMeshPro.isActiveAndEnabled) {
+                    textMeshPro.text += message[charIndex];
+                }
+                else if (textUI.isActiveAndEnabled) {
+                    textUI.text += message[charIndex];
+                }
+                charIndex++;
+                yield return new WaitForSeconds(0.05f/speed);
+            }
+        }
+        yield return null;
+    }
+}
