7e1588cdfc0e2d107aa74bd6b0536f5fdc42b8f9
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-01-30 21:04:59 +0200

    Falling Platform
    Falling platform prefab was added with its script. It has 2 variables. A delay of the fall (in seconds) after impact with the player and a death delay after the fall.

---
+++ Assets/Scripts/LevelDesign/Platform/FallOnTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOnTouch : MonoBehaviour
+{
+    public float falldelay = 0f;
+    public float deathDelayAfterFall=2f;
+    Rigidbody rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Fall(){
+        if (rig){
+            rig.isKinematic = false;
+            rig.useGravity = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.tag=="PlayerHuman" || other.tag=="PlayerDog"){
+            StartCoroutine(DelayFall());
+        }
+    }
+
+    IEnumerator DelayFall(){
+        if (falldelay>0) yield return new WaitForSeconds(falldelay);
+        Fall();
+        yield return new WaitForSeconds(deathDelayAfterFall);
+        Destroy(gameObject);
+        yield return null;
+    }
+}
