25491d2c0eff099f86a7265fc88675efbb891533
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-01 18:05:01 +0200

    BrainDeadFollower enemy
    Its a basic enemy that patrols on the given relative points.
     "Enemy" tag was added

---
+++ Assets/Scripts/Enemies/EnemyFollowPoints.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPoints : MonoBehaviour
+{
+    public Vector3[] points;
+    public int pointIndex;
+    Vector3 currentTarget;
+    public float tolerance;
+    public float speed=1f;
+    public float delayTime;
+    float delayStart;
+    public bool automatic=true;
+    Rigidbody rig;
+
+    void Start(){
+        rig = GetComponent<Rigidbody>();
+        if (points.Length > 0){
+            currentTarget = points[0];
+        }
+        tolerance = speed * Time.fixedDeltaTime;
+    }
+
+    void FixedUpdate(){
+        if (transform.localPosition!=currentTarget){
+            MovePlatform();
+        }
+        else {
+            UpdateTarget();
+        }
+    }
+
+    void MovePlatform(){
+        Vector3 heading = currentTarget - transform.localPosition;
+        rig.MovePosition(transform.position + heading.normalized * speed * Time.fixedDeltaTime);
+
+        if (heading.magnitude < tolerance){
+            transform.localPosition = currentTarget;
+            delayStart = Time.time;
+        }
+    }
+
+    void UpdateTarget(){
+        if (automatic){
+            if (Time.time - delayStart > delayTime){
+                NextPoint();
+            }
+        }
+    }
+
+    public void NextPoint(){
+        pointIndex++;
+        if (pointIndex >= points.Length) pointIndex = 0;
+        currentTarget = points[pointIndex];
+    }
+}
