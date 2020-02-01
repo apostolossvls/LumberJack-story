47994ac97fa7567391c434a890af5323eb04a339
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-01 19:34:49 +0200

    NearGroundFollower enemy
    This enemy has a vision radius, which is specified by a trigger collider. Then a playable character enters its vision it start to follow him on a NavMeshSurface. Upon exit the enemy stays stationary.

---
+++ Assets/Scripts/Enemies/EnemyNavMeshFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMeshFollow : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+    public float sphereRadius = 0.5f;
+    public float viewDistance = 5f;
+    public string[] tags;
+    public LayerMask layerMask;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target) {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (TagMatch(other.tag) && !target){
+            target = other.transform;
+        }
+    }
+    void OnTriggerExit(Collider other){
+        if (target){
+            if (target==other.transform){
+                target = null;
+            }
+        }
+    }
+
+    bool TagMatch (string s){
+        foreach (string t in tags)
+        {
+            if (s==t) return true;
+        }
+        return false;
+    }
+}
