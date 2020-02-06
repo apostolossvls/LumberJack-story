5c423283f02443d6b847387cd9ff9eb26984e403
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-06 19:23:23 +0200

    Main menu
    Main menu scene was added. It is nothing but an example of loading different scenes async and UI navigation. 3 scenes were added to build settings (mainmenu, sample and test level 1).

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static MainMenuManager Instance;
+    public Transform target;
+    public Transform startPanelTransform;
+    public Transform scenePanelTransform;
+    void Awake(){
+        if (Instance!=null && Instance!=this){
+            Destroy(this.gameObject);
+        }
+        else {
+            Instance = this;
+        }
+    }
+
+    public void GoToStart(){
+        target.SetPositionAndRotation(startPanelTransform.position, startPanelTransform.rotation);
+    }
+
+    public void GoToScenePanel(){
+        target.SetPositionAndRotation(scenePanelTransform.position, scenePanelTransform.rotation);
+    }
+
+    public void LoadScene(int index){
+        SceneManager.LoadSceneAsync(index);
+    }
+}
