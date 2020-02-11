9f891958021fed51ea58d151845d5e3c40415dec
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-11 14:51:38 +0200

    Camera target follow rewritten
    The pivot that follows the target is clamping withing its target position and its boundaries in X and Y axis.

--- Assets/Scripts/LevelDesign/CameraFollowPivot.cs
+++ Assets/Scripts/LevelDesign/CameraFollowPivot.cs
@@ -10,71 +10,73 @@ public class CameraFollowPivot : MonoBehaviour
     public Collider[] boundaries;
     CinemachineVirtualCamera vcam;
     CinemachineConfiner confiner;
+    Collider currentCollider;
 
     void Awake(){
         if (target){
             vcam = cam.GetComponent<CinemachineVirtualCamera>();
             confiner = vcam.GetComponent<CinemachineConfiner>();
-            //Vector3 v = new Vector3(target.position.x, target.position.y, cam.position.z);
-            for (int i = 0; i < boundaries.Length; i++)
-            {
-
-            if (!boundaries[i]) continue; 
-
-            if (boundaries[i].bounds.Contains(target.position)){
-                transform.position = target.position;
-                CameraBoundary cb = boundaries[i].GetComponent<CameraBoundary>();
-                if (cb) confiner.m_BoundingVolume = cb.cameraBoundary;
-                else Debug.Log(this+": Error confiner.m_BoundingVolume missing");
-                break;
-            }
-            else {
-                if (boundaries[i].bounds.Contains(new Vector3(target.position.x, boundaries[i].transform.position.y, target.position.z))){
-                    //Debug.Log("IN X");
-                    transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
-                }
-                if (boundaries[i].bounds.Contains(new Vector3(boundaries[i].transform.position.x, target.position.y, target.position.z))){
-                    //Debug.Log("IN Y");
-                    transform.position = new Vector3(transform.position.x, target.position.y, target.position.z);
-                }
-            }
-
-            }
         }
     }
 
     void Update()
     {
-        if (target){
-            //Vector3 v = new Vector3(target.position.x, target.position.y, cam.position.z);
-            for (int i = 0; i < boundaries.Length; i++)
-            {
+        for (int i = 0; i < boundaries.Length; i++)
+        {
 
-            if (!boundaries[i]) continue; 
+        if (!boundaries[i]) continue; 
 
-            if (boundaries[i].bounds.Contains(target.position)){
-                transform.position = target.position;
-                CameraBoundary cb = boundaries[i].GetComponent<CameraBoundary>();
-                if (cb) confiner.m_BoundingVolume = cb.cameraBoundary;
-                else Debug.Log(this+": Error confiner.m_BoundingVolume missing");
-                break;
-            }
-            else {
-                if (boundaries[i].bounds.Contains(new Vector3(target.position.x, boundaries[i].transform.position.y, target.position.z))){
-                    //Debug.Log("IN X");
-                    transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
-                }
-                if (boundaries[i].bounds.Contains(new Vector3(boundaries[i].transform.position.x, target.position.y, target.position.z))){
-                    //Debug.Log("IN Y");
-                    transform.position = new Vector3(transform.position.x, target.position.y, target.position.z);
+        if (boundaries[i].bounds.Contains(target.position)){
+            
+            CameraBoundary cb = boundaries[i].GetComponent<CameraBoundary>();
+            if (cb) {
+                currentCollider = cb.cameraBoundary;
+                if (confiner.m_BoundingVolume != cb.cameraBoundary){
+                    confiner.m_BoundingVolume = cb.cameraBoundary;
                 }
             }
+            else Debug.Log(this+": Error confiner.m_BoundingVolume missing");
+            break;
+        }
 
+        }
+
+        if (currentCollider){
+            Magnetize(currentCollider);
+            /*
+            Debug.Log("col: "+currentCollider);
+        if (currentCollider.bounds.Contains(new Vector3(target.position.x, target.position.y, currentCollider.transform.position.z))){
+            transform.position = target.position;
+            Debug.Log("IN X,Y");
+        }
+        else {
+            if (currentCollider.bounds.Contains(new Vector3(target.position.x, currentCollider.transform.position.y, currentCollider.transform.position.z))){
+                Debug.Log("IN X");
+                transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
             }
+            if (currentCollider.bounds.Contains(new Vector3(currentCollider.transform.position.x, target.position.y, currentCollider.transform.position.z))){
+                Debug.Log("IN Y");
+                transform.position = new Vector3(transform.position.x, target.position.y, target.position.z);
+            }
+        }
+        */
+
         }
     }
 
     public void ChangeTarget(Transform t){
         target = t;
+    }
+
+    void ResetPosition(){
+        transform.position = target.position;
+    }
+
+    void Magnetize(Collider col){
+        transform.position = new Vector3(
+            Mathf.Clamp(Mathf.Abs(target.position.x), float.MinValue, col.bounds.extents.x + Mathf.Abs(col.transform.position.x)) * Mathf.Sign(target.position.x),
+            Mathf.Clamp(Mathf.Abs(target.position.y), float.MinValue, col.bounds.extents.y + Mathf.Abs(col.transform.position.y)) * Mathf.Sign(target.position.y),
+            target.position.z
+        );
     }
 }
