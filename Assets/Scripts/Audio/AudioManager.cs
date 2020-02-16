0fc23a515f5ecdb94605d19c11cbdb002061ad74
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-16 22:22:10 +0200

    Audio Manager
    "Sound" class was added.
    AudioManager has a list of "Sound". On awake it creates an audio source for every element of the list, tranfering the variables. The clip of the audio source is taken from the files.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Sound[] sounds;
+    public Transform parent;
+
+    void Awake(){
+        foreach (Sound s in sounds)
+        {
+            s.source = parent.gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play (string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s==null) {
+            Debug.LogWarning("Sound: "+name+" not found");
+            return;
+        } 
+        s.source.Play();
+    }
+
+    public void BypassEffectsMusic (bool f){
+        if (f) audioMixer.SetFloat("MusicDriMix", 0);
+        else audioMixer.SetFloat("MusicDriMix", 100);
+    }
+}
