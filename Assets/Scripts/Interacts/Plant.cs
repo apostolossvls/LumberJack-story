e230fad3f168d219d43e5fc298319847d6dfd2f2
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-19 23:21:48 +0200

    Torch and plants vs fire
    Added "Plant" tag. plants are vulnerable to fire.
    Torch prefab was added.

---
+++ Assets/Scripts/Interacts/Plant.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public float power=1f;
+    public bool isTrigger = false;
+
+    void Update(){
+        if (power<=0){
+            Die();
+        }
+    }
+
+    public void SetPower(float amount, bool fixedNumber=false){
+        if (!fixedNumber){
+            power += amount;
+        }
+        else {
+            power = amount;
+        }
+    }
+
+    public void Die(){
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Fire"){
+            SetPower(-1 * Time.deltaTime);
+        }
+    }
+}
