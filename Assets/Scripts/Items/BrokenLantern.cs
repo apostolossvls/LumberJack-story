2ec4c26e4bd943d86e62122c61806d63f7e53ea7
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-23 23:18:23 +0200

    Lantern that breaks
    "BrokenLantern" script was added.
    "BrokenLantern" prefab was added.
    This lantern is simular to the previous lantern but it breaks into parts  upon big impact.

---
+++ Assets/Scripts/Items/BrokenLantern.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenLantern : MonoBehaviour
+{
+    public float forceToBreak = 8f;
+    public float delayAfterBreak = 5f;
+    public GameObject[] parts;
+
+    void OnCollisionEnter(Collision other){
+        if (other.relativeVelocity.magnitude > forceToBreak){
+            Debug.Log("big impact");
+            Impact();
+        }
+    }
+
+    void Impact(){
+        foreach (GameObject part in parts)
+        {
+            Rigidbody r = part.GetComponent<Rigidbody>();
+            if (!r) {
+                r = part.AddComponent<Rigidbody>();
+                r.mass = 0.1f;
+            }
+            else if (r.isKinematic){
+                r.isKinematic = false;
+            }
+            part.GetComponent<Collider>().isTrigger = false;
+            part.transform.SetParent(null);
+            Destroy(part, delayAfterBreak);
+        }
+        Destroy(gameObject, delayAfterBreak);
+    }
+}
