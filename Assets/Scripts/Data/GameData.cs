49e449a162a7f30f8af193249e1dd6ba111a6834
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-02-24 22:55:03 +0200

    Save/Load system tests
    This saves and load objects to a file  on the game path. Important variables will be stored in "GameData" and this object will be saved/loaded.

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int counter;
+
+    public GameData(int c) {
+        counter = c;
+    }
+}
---
+++ Assets/Scripts/Data/SaveManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+    [HideInInspector] public string m_Path;
+    public int counter = 0;
+    public bool InspectorTriggerSave;
+    public bool InspectorTriggerLoad;
+
+    void Awake(){
+        if (instance == null)
+            instance = this;
+        else{
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        m_Path = Application.dataPath;
+        Debug.Log("dataPath : " + m_Path);
+    }
+
+    void Update(){
+        if (InspectorTriggerSave){
+            InspectorTriggerSave = false;
+            SaveFile();
+        }
+        if (InspectorTriggerLoad){
+            InspectorTriggerLoad = false;
+            LoadFile();
+        }
+    }
+
+    public void SaveFile()
+    {
+        string destination = m_Path + "/save.dat";
+        FileStream file;
+
+        if(File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+
+        GameData data = new GameData(counter);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadFile()
+    {
+        string destination = m_Path + "/save.dat";
+        FileStream file;
+
+        if(File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            Debug.LogError("File not found");
+            return;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        GameData data = (GameData) bf.Deserialize(file);
+        file.Close();
+
+        counter = data.counter;
+    }
+}
