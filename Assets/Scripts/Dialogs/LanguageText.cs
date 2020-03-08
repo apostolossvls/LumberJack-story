a500e9c78138fc11396b769eff3e1baf0237ec55
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-08 11:42:33 +0200

    Text translation
    "LanguageText" script can be added to every text component. It contains an array of string for every availiable translation of this text.
    0) English (default).
    1) Greek
    Our goal is to make the game availiable to as many groups as possible.

---
+++ Assets/Scripts/Dialogs/LanguageText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LanguageText : MonoBehaviour
+{
+    [TextArea(5, 20)]
+    public string[] text;
+
+    void Start() {
+        Setup();
+    }
+
+    public void Setup() {
+        if (PauseMenuManager.instance)
+        {
+            int l = PauseMenuManager.instance.languageIndex;
+            if (l >= 0)
+            {
+                Text t0 = GetComponent<Text>();
+                if (t0)
+                {
+                    t0.text = text[l];
+                    return;
+                }
+                TextMeshPro t1 = GetComponent<TextMeshPro>();
+                if (t1)
+                {
+                    t1.text = text[l];
+                    return;
+                }
+                TextMeshProUGUI t2 = GetComponent<TextMeshProUGUI>();
+                if (t2)
+                {
+                    t2.text = text[l];
+                    return;
+                }
+            }
+        }
+    }
+}
